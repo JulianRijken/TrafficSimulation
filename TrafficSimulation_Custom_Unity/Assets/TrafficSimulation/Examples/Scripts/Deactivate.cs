d5d480eeb6f19c2502de7aab362b69f7deb601e0
Julian <mail@julianrijken.com>
2024-12-06 17:41:23 +0100

    feat: Added base project

---
+++ TrafficSimulation_Custom_Unity/Assets/TrafficSimulation/Examples/Scripts/Deactivate.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TrafficSimulation;
+
+public class Deactivate : MonoBehaviour
+{
+    bool isActive = true;
+
+    GameObject[] vehicles;
+    TrafficSystem ts;
+
+    private void Start()
+    {
+        vehicles = GameObject.FindGameObjectsWithTag("AutonomousVehicle");
+        ts = GameObject.FindObjectOfType<TrafficSystem>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            if(isActive)
+            {
+                isActive = false;
+                ts.SaveTrafficSystem();
+                foreach(GameObject vehicle in vehicles){
+                    vehicle.SetActive(false);
+                }
+            }
+            else
+            {
+                isActive = true;
+
+                foreach(GameObject vehicle in vehicles){
+                    vehicle.SetActive(true);
+                    
+                }
+                
+                ts.ResumeTrafficSystem();
+            }
+        }
+    }
+}
